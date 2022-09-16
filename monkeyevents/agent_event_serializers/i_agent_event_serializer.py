856789e783e5f480b412b5080047e1bd522dbfd4
Shreya Malviya <shreya.malviya@gmail.com>
2022-09-16 12:52:55 +0530

    Rename event_serializers/ -> agent_event_serializers/
    Original commit https://github.com/guardicore/monkey/commit/d7327272a08016c222e5e452a2f9a902e2016c72

---
+++ monkeyevents/agent_event_serializers/i_agent_event_serializer.py
@@ -0,0 +1,43 @@
+from abc import ABC, abstractmethod
+from typing import Dict, List, Union
+
+from monkeyevents.events import AbstractAgentEvent
+
+JSONSerializable = Union[  # type: ignore[misc]
+    Dict[str, "JSONSerializable"],  # type: ignore[misc]
+    List["JSONSerializable"],  # type: ignore[misc]
+    int,
+    str,
+    float,
+    bool,
+    None,
+]
+
+
+class IAgentEventSerializer(ABC):
+    """
+    Manages serialization and deserialization of events
+    """
+
+    @abstractmethod
+    def serialize(self, event: AbstractAgentEvent) -> JSONSerializable:
+        """
+        Serializes an event
+
+        :param event: Event to serialize
+        :return: Serialized event
+        """
+        pass
+
+    @abstractmethod
+    def deserialize(self, serialized_event: JSONSerializable) -> AbstractAgentEvent:
+        """
+        Deserializes an event
+
+        :param serialized_event: Serialized event to deserialize
+        :return: Deserialized event
+        :raises TypeError: If one or more of the serialized fields contains data of an incompatible
+                           type
+        :raises ValueError: If one or more of the serialized fields contains an incompatible value
+        """
+        pass
